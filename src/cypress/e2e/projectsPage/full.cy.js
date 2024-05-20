694ed0f1b7191b801229b40893b3046ac32d26d1
Caio <caioshadow2014@gmail.com>
2024-05-20 14:48:31 -0300

    Conserta testes na página de espaços e testes na página de projetos

--- src/cypress/e2e/projectsPage/full.cy.js
+++ src/cypress/e2e/projectsPage/full.cy.js
@@ -8,8 +8,8 @@ describe("Pagina de Projetos", () => {
 
     it("clica em \"Acessar\" e entra na pagina no projeto selecionado", () => {
         cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
-        cy.url().should("include", "/projeto/");
-        cy.contains('h1', 'MinC');
+        cy.url().should("include", "/projeto/19/#info");
+        cy.contains('h1', 'Oficina');
     });
 
     it("Garante que o botão limpar filtros na pagina de projetos funciona", () => {
@@ -17,28 +17,6 @@ describe("Pagina de Projetos", () => {
             ".verified",
             ".mc-multiselect--input",
             ":nth-child(1) > .mc-multiselect__option"
-        ]);
-
-        cy.wait(1000);
-
-        let countBeforeClear;
-        cy.get('.foundResults').then(($foundResults) => {
-            let resultsTextArray;
-            resultsTextArray = $foundResults.text().split(" ");
-            countBeforeClear = Number(resultsTextArray[0]);
-        });
-
-        cy.get('.foundResults').then(($foundResults) => {
-            let resultsTextArray, resultsCount;
-
-            resultsTextArray = $foundResults.text().split(" ");
-            resultsCount = Number(resultsTextArray[0]);
-            
-            cy.get(".upper.project__color").should("have.length", resultsCount);
-            cy.wait(1000);
-            cy.get(".upper.project__color").should("have.length", countBeforeClear);
-            cy.wait(1000);
-            cy.contains(resultsCount + " Projetos encontrados");
-        });
+        ], "19 Projetos encontrados");
     });
 });
--- src/cypress/e2e/projectsPage/index.cy.js
+++ src/cypress/e2e/projectsPage/index.cy.js
@@ -4,10 +4,11 @@ describe("Projeto", () => {
         cy.get(".mc-header-menu__btn-mobile").click();
         cy.contains(".mc-header-menu__itens a", "Projetos").click();
         cy.url().should("include", "projetos");
-        cy.get(".search-filter__actions--form-input").type("Festa Junina");
+        cy.get(".search-filter__actions--form-input").type("projeto");
+        cy.get(".search-filter__actions--form-button").click();
         cy.wait(1000);
-        cy.get('[href="https://mapas.tec.br/projeto/9/"]').last().click();
+        cy.get(':nth-child(3) > .entity-card__footer > .entity-card__footer--action > .button').click();
         cy.wait(1000);
-        cy.url().should("include", "/projeto/");
+        cy.contains('p', 'teste projeto concurso');
     });
 });

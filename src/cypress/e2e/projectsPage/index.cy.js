753e01345d23c391d36bfc78dd6e42430a1bcbc5
Caio <caioshadow2014@gmail.com>
2024-05-20 21:06:39 -0300

    Remove 'beforeEach' em 'mapas/src/cypress/e2e/projectsPage/index.cy.js'

---
+++ src/cypress/e2e/projectsPage/index.cy.js
@@ -0,0 +1,14 @@
+describe("Projeto", () => {
+    it("Garante que o projeto seja clicável", () => {
+        cy.visit("/");
+        
+        cy.get(".mc-header-menu__btn-mobile").click();
+        cy.contains(".mc-header-menu__itens a", "Projetos").click();
+        cy.url().should("include", "projetos");
+        cy.get(".search-filter__actions--form-input").type("Festa Junina");
+        cy.wait(1000);
+        cy.get('[href="https://mapas.tec.br/projeto/9/"]').last().click();
+        cy.wait(1000);
+        cy.url().should("include", "/projeto/");
+    });
+});

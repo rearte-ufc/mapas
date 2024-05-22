2c047ef9660041f095867717c9d8d643fe69b5f5
Lucas Pirola <lucaspirola@gmail.com>
2024-05-22 12:59:18 -0300

    Pequenos ajustes para carregar o App pelo phpunit

--- src/core/Entities/Opportunity.php
+++ src/core/Entities/Opportunity.php
@@ -9,6 +9,8 @@ use MapasCulturais\Traits;
 use MapasCulturais\App;
 use MapasCulturais\Definitions\Metadata as MetadataDefinition;
 /**
+ *
+ *
  * Opportunity
  *
  * @property-read int $id
@@ -20,8 +22,8 @@ use MapasCulturais\Definitions\Metadata as MetadataDefinition;
  * @property \DateTime $publishTimestamp
  * @property-read boolean $publishedRegistrations
  * @property-read int $totalRegistrations 
- * 
- * 
+ *
+ *
  * @property string $name
  * @property string $shortDescription
  * @property \DateTime $registrationFrom
@@ -31,13 +33,13 @@ use MapasCulturais\Definitions\Metadata as MetadataDefinition;
  * @property array $registrationRanges
  * @property self $parent
  * @property Agent $owner
- * 
- * 
+ *
+ *
  * @property EvaluationMethodConfiguration $evaluationMethodConfiguration
  * @property RegistrationFileConfiguration $registrationFileConfigurations
  * @property RegistrationFieldConfiguration $registrationFieldConfigurations
  * @property \MapasCulturais\Entity $ownerEntity
- * 
+ *
  *
  * @ORM\Table(name="opportunity", indexes={
  *      @ORM\Index(name="opportunity_entity_idx", columns={"object_type", "object_id"}),
@@ -77,7 +79,7 @@ abstract class Opportunity extends \MapasCulturais\Entity
         Traits\EntityArchive{
             Traits\EntityNested::setParent as nestedSetParent;
         }
-        
+
     protected $__enableMagicGetterHook = true;
     protected $__enableMagicSetterHook = true;
 
@@ -485,7 +487,7 @@ abstract class Opportunity extends \MapasCulturais\Entity
     }
 
     static function getClassName() {
-        return Opportunity::class;
+        return static::class;
     }
 
     function getExtraPermissionCacheUsers(){

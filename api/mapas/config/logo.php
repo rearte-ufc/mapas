ddddfc77860da247b0e8f098901c64f9f2ddf810
Enieber Cunha <eniebercunha@gmail.com>
2024-06-19 16:04:20 -0300

    feat: alterado thema base v2

--- api/mapas/config/logo.php
+++ api/mapas/config/logo.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 use \MapasCulturais\i;
 
 return [
     /* Primeira linha do logo configurável */
-    'logo.title' => env('LOGO_TITLE', i::__('Mapas')),
+    'logo.title' => '',
 
     /* Segunda linha do logo configurável */
-    'logo.subtitle' => env('LOGO_SUBTITLE', i::__('Culturais')),
+    'logo.subtitle' => '',
 
     /* Cores da logo */
     'logo.colors' => [
@@ -16,12 +16,12 @@ return [
         "var(--mc-secondary-500)",
     ],
 
-    /* 
+    /*
     Define o nome do asset da imagem da logo do site - Substituirá a logo padrão
 
-    ex: `img/meu-mapa-logo.jpg` (pasta assets/img/meu-mapa-logo.jpg do tema) 
+    ex: `img/meu-mapa-logo.jpg` (pasta assets/img/meu-mapa-logo.jpg do tema)
     */
-    'logo.image' => env('LOGO_IMAGE', ''),
+    'logo.image' => env('LOGO_IMAGE', 'img/logo-mapas.png'),
 
     /* Esconde o título e subtitulo */
     'logo.hideLabel' => env('LOGO_HIDELABEL', false),

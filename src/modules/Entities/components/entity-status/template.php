c49ed577b92f1f9dfa34fa38a75c22370e03b2cf
gabriela <gabriela.gandine@gmail.com>
2024-07-16 10:15:46 -0300

    palavra você duplicada corrigida

--- src/modules/Entities/components/entity-status/template.php
+++ src/modules/Entities/components/entity-status/template.php
@@ -75,7 +75,7 @@ $this->import('
 
         <template v-if="entity.__objectType == 'opportunity'">
             <span v-if="entity.status == 0">
-                <strong><?= i::__('Esta oportunidade está em rascunho.'); ?></strong>
+                <strong><?= i::__('Este oportunidade está em rascunho.'); ?></strong>
                 <?= i::__('Você precisa <strong>publicar</strong> para exibir para todas as pessoas.') ?>
             </span>
             <span v-if="entity.status == -10">

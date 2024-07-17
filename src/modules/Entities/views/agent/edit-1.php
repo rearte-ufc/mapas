c989f81d0bc58059eeede497b176693d7b6636bd
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-07-17 14:39:41 -0300

    feat: refatora subtitle conteúdo card

--- src/modules/Entities/views/agent/edit-1.php
+++ src/modules/Entities/views/agent/edit-1.php
@@ -68,10 +68,11 @@ $this->breadcrumb = [
                             <entity-field :entity="entity" classes="col-12" prop="name" label="<?php i::_e('Nome de perfil') ?>"></entity-field>
                             <?php $this->applyTemplateHook('entity-info','end') ?>
 
-                            <div class="grid-12">
-                                <entity-terms :entity="entity" taxonomy="funcao" editable classes="col-12" title="<?php i::_e('Função(ões) na cultura'); ?>"></entity-terms>
-                                <entity-terms :entity="entity" taxonomy="tag" classes="col-12" title="Tags" editable></entity-terms>                                
-                            <!-- </div> -->
+                            <?php $this->applyTemplateHook('edit1-entity-info-taxonomie-area','before') ?>
+                            <entity-terms :entity="entity" taxonomy="area" editable classes="col-12" title="<?php i::_e('Área(s) de atuação *'); ?>"></entity-terms>
+                            <?php $this->applyTemplateHook('edit1-entity-info-taxonomie-area','after') ?>
+                            <entity-terms :entity="entity" taxonomy="funcao" editable classes="col-12" title="<?php i::_e('Função(ões) na cultura'); ?>"></entity-terms>
+                            <entity-terms :entity="entity" taxonomy="tag" classes="col-12" title="Tags" editable></entity-terms>                                
                         
                             <?php $this->applyTemplateHook('edit1-entity-info-shortDescription','before') ?>
                             <entity-field :entity="entity" classes="col-12" prop="shortDescription" label="<?php i::_e('Descrição curta') ?>"></entity-field>
@@ -91,70 +92,58 @@ $this->breadcrumb = [
                         </div>
                     </template>
                 </mc-card>
-                    <mc-card class="feature__full">
-                        <template #title>
-                            <h3 class="bold"><?php i::_e("Dados Pessoais"); ?></h3>
-                            <p><?php i::_e("Os dados inseridos abaixo não serão exibidos publicamente, exceto os casos em que forem selecionadas as opções ”Mostrar no perfil”."); ?></p>
-                        </template>
-                        <template #content>
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-6" prop="nomeSocial" label="<?= i::__('Nome Social') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6" prop="nomeCompleto" label="<?= i::__('Nome Completo') ?>"></entity-field>
-                                <entity-field v-if="global.auth.is('admin')" :entity="entity" prop="type" @change="entity.save(true).then(() => global.reload())" classes="col-12"></entity-field>
-                                <entity-field :entity="entity" classes="col-4" prop="cpf"></entity-field>
-                                <!-- <entity-field :entity="entity" classes="col-12" prop="cnpj" label="<?= i::__('MEI (CNPJ do MEI)') ?>"></entity-field> -->
-                                <entity-field :entity="entity" classes="col-4" prop="emailPrivado" label="<?= i::__('E-mail pessoal') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-4" prop="telefone1" label="<?= i::__('Telefone pessoal (com DDD)') ?>"></entity-field>
-                                <!-- <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone2" label="<?= i::__('Telefone privado 2 com DDD') ?>"></entity-field> -->
-                                <div class="col-12 divider"></div>
-                                <entity-location :entity="entity" classes="col-12" editable></entity-location>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="agenteItinerante" label="<?= i::__('É agente itinerante?') ?>"></entity-field>
-                            </div>
-                            <div class="col-12">
-                                <h3>Dados bancário</h3>
-                            </div>
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_account_type"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_number"></entity-field>
-                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_account_number"></entity-field>
-                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_account_number"></entity-field>
-                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_branch"></entity-field>
-                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_branch"></entity-field>
-                            </div>
-                        </template>
-                    </mc-card>
-                    <mc-card class="feature__full">
-                        <template #title>
-                            <h3 class="bold"><?php i::_e("Dados sensíveis"); ?></h3>
-                            <p class="data-subtitle"><?php i::_e("Os campos em que não forem selecionadas a opção ”Ocultar do perfil” serão exibidos para todos os usuários da plataforma."); ?></p>
-                        </template>
-                        <template #content>
-                            <div class="grid-12">
-                                <entity-field :entity="entity" classes="col-4 sm:col-12" prop="dataDeNascimento" label="<?= i::__('Data de Nascimento') ?>"></entity-field>
-                                <!-- <div class="field col-6">
-                                    <label>{{entity.$PROPERTIES['idoso'].label}}</label>
-                                    <input type="text" disabled :value="entity.idoso ? 'Sim' : 'Não'" />
-                                </div>    
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="genero" label="<?= i::__('Selecione o Gênero') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="orientacaoSexual" label="<?= i::__('Selecione a Orientação Sexual') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="raca" label="<?= i::__('Selecione a Raça/Cor') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="escolaridade" label="<?= i::__('Selecione a sua Escolaridade') ?>"></entity-field>
-                              
-                                <entity-field :entity="entity" classes="col-12" prop="pessoaDeficiente" class="pcd col-12" label="<?= i::__('Pessoa com Deficiência') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="comunidadesTradicional" label="<?= i::__('Pertence a comunidade tradicional') ?>"></entity-field>
-                                <entity-field :entity="entity" classes="col-12" prop="comunidadesTradicionalOutros" label="<?= i::__('Não encontrou sua comunidade Tradicional') ?>"></entity-field>
-                            </div>
-                        </template>
-                    </mc-card>
-                    <mc-card class="feature__full">
-                        <template #title>
-                            <h3 class="bold"><?php i::_e("Redes sociais"); ?></h3>
-                            <p class="data-subtitle"><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários da plataforma."); ?></p>
-                        </template>
-                        <template #content>
-                            <div class="grid-12">
-                            <!-- <entity-field classes="col-6" :entity="entity" prop="instagram"></entity-field>
-                            <entity-field classes="col-6" :entity="entity" prop="instagram"></entity-field> -->
+
+                <mc-card class="feature__full">
+                    <template #title>
+                        <h3 class="bold"><?php i::_e("Dados Pessoais"); ?></h3>
+                        
+                    </template>
+                    <template #content>
+                        <div class="grid-12">
+                            <p class="col-12 mc-card__card_info"><?php i::_e("Os dados inseridos abaixo não serão exibidos publicamente, exceto os casos em que forem selecionadas as opções ”Mostrar no perfil”."); ?></p>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="nomeSocial" label="<?= i::__('Nome Social') ?>"></entity-field>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="nomeCompleto" label="<?= i::__('Nome Completo') ?>"></entity-field>
+                            <entity-field v-if="global.auth.is('admin')" :entity="entity" prop="type" @change="entity.save(true).then(() => global.reload())" classes="col-12 sm:col-12"></entity-field>
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="cpf"></entity-field>
+                            <!-- <entity-field :entity="entity" classes="col-12" prop="cnpj" label="<?= i::__('MEI (CNPJ do MEI)') ?>"></entity-field> -->
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="emailPrivado" label="<?= i::__('E-mail pessoal') ?>"></entity-field>
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="telefone1" label="<?= i::__('Telefone pessoal (com DDD)') ?>"></entity-field>
+                            <!-- <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone2" label="<?= i::__('Telefone privado 2 com DDD') ?>"></entity-field> -->
+                            <div class="divider col-12"></div>
+                            <entity-location :entity="entity" classes="col-12 sm:col-12" editable></entity-location>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="agenteItinerante" label="<?= i::__('É agente itinerante?') ?>"></entity-field>
+                        </div>
+                        <div class="divider"></div>
+                        <h4>Dados bancário</h4>
+                       
+                        <div class="grid-12">
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_account_type"></entity-field>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_number"></entity-field>
+                            <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_account_number"></entity-field>
+                            <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_account_number"></entity-field>
+                            <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_branch"></entity-field>
+                            <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_branch"></entity-field>
+                        </div>
+                    </template>
+                </mc-card>
+
+                <mc-card class="feature__full">
+                    <template #title>
+                        <h3 class="bold"><?php i::_e("Dados sensíveis"); ?></h3>
+                    </template>
+                    <template #content>
+                        <div class="grid-12">
+                            <p class="col-12 mc-card__card_info"><?php i::_e("Os campos em que não forem selecionadas a opção ”Ocultar do perfil” serão exibidos para todos os usuários da plataforma."); ?></p>
+
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="dataDeNascimento" label="<?= i::__('Data de Nascimento') ?>"></entity-field>
+                            <!-- <div class="field col-6">
+                                <label>{{entity.$PROPERTIES['idoso'].label}}</label>
+                                <input type="text" disabled :value="entity.idoso ? 'Sim' : 'Não'" />
+                            </div>     -->
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="genero" label="<?= i::__('Gênero') ?>"></entity-field>
+                            <entity-field :entity="entity" classes="col-4 sm:col-12" prop="orientacaoSexual" label="<?= i::__('Orientação Sexual') ?>"></entity-field>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="raca" label="<?= i::__('Raça/Cor') ?>"></entity-field>
+                            <entity-field :entity="entity" classes="col-6 sm:col-12" prop="escolaridade" label="<?= i::__('Escolaridade') ?>"></entity-field>
                             
                             <entity-social-media :entity="entity" editable></entity-social-media>
                             </div>

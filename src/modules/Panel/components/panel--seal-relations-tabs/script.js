c638a557ac4ffaa932684fcdbc927a672041fb37
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2024-07-25 09:11:43 -0300

    ADD - panel--seal-relations-tabs

---
+++ src/modules/Panel/components/panel--seal-relations-tabs/script.js
@@ -0,0 +1,15 @@
+app.component("panel--seal-relations-tabs", {
+	template: $TEMPLATES["panel--seal-relations-tabs"],
+	emits: [],
+
+	setup() {},
+
+	created() {},
+
+	data() {},
+	computed: {},
+
+	props: {},
+
+	methods: {},
+});
---
+++ src/modules/Panel/components/panel--seal-relations-tabs/template.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ */
+
+use MapasCulturais\i;
+?>
+oi

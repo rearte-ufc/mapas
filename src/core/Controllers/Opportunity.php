d13bc9281123f21bd850d6503751fcb69207e8a7
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-07-25 11:02:44 -0300

    feat: adiciona no modelo o metalist incluindo grupo videos e links

--- src/core/Controllers/Opportunity.php
+++ src/core/Controllers/Opportunity.php
@@ -1403,6 +1403,15 @@ class Opportunity extends EntityController {
             $fileConfiguration->save(true);
         }
 
+        foreach ($opportunity->getMetaLists() as $metaList_) {
+            foreach ($metaList_ as $metaList__) {
+                $metalist = $metaList__;
+                $metalist->setOwner($newOpportunity);
+            
+                $metalist->save(true);
+            }
+        }
+
         $newOpportunity->save();
 
         if($this->isAjax()){

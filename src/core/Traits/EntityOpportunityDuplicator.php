a52cc2ff2257268153aab4ffcb92c619179f2636
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-07-29 12:27:46 -0300

    feat: aplica regra para agent relations

--- src/core/Traits/EntityOpportunityDuplicator.php
+++ src/core/Traits/EntityOpportunityDuplicator.php
@@ -6,57 +6,62 @@ use MapasCulturais\Entities\ProjectOpportunity;
 use MapasCulturais\Entity;
 
 trait EntityOpportunityDuplicator {
+
+    private ProjectOpportunity $opportunity;
+    private ProjectOpportunity $newOpportunity;
+
     function ALL_duplicate(){
         $app = App::i();
 
         $this->requireAuthentication();
-        $opportunity = $this->requestedEntity;
-        
-        $newOpportunity = $this->cloneOpportunity($opportunity);
-        
-        $this->duplicateEvaluationMethods($opportunity, $newOpportunity);
-        $this->duplicatePhases($opportunity, $newOpportunity);
-        $this->duplicateMetadata($opportunity, $newOpportunity);
-        $this->duplicateRegistrationFieldsAndFiles($opportunity, $newOpportunity);
-        $this->duplicateMetalist($opportunity, $newOpportunity);
-        $this->duplicateFiles($opportunity, $newOpportunity);
+        $this->opportunity = $this->requestedEntity;
+        $this->newOpportunity = $this->cloneOpportunity();
 
-        $newOpportunity->save();
+        $this->duplicateEvaluationMethods();
+        $this->duplicatePhases();
+        $this->duplicateMetadata();
+        $this->duplicateRegistrationFieldsAndFiles();
+        $this->duplicateMetalist();
+        $this->duplicateFiles();
+        $this->duplicateAgentRelations();
+
+        $this->newOpportunity->save(true);
        
         if($this->isAjax()){
-            $this->json($opportunity);
+            $this->json($this->opportunity);
         }else{
             $app->redirect($app->request->getReferer());
         }
     }
 
-    private function cloneOpportunity(ProjectOpportunity $opportunity) : ProjectOpportunity
+    private function cloneOpportunity() : ProjectOpportunity
     {
         $app = App::i();
 
-        $newOpportunity = clone $opportunity;
+        $this->newOpportunity = clone $this->opportunity;
 
         $dateTime = new \DateTime();
         $now = $dateTime->format('d-m-Y H:i:s');
-        $newOpportunity->setName("$opportunity->name  - [Cópia][$now]");
-        $newOpportunity->setStatus(Entity::STATUS_DRAFT);
-        $app->em->persist($newOpportunity);
+        $name = $this->opportunity->name;
+        $this->newOpportunity->setName("$name  - [Cópia][$now]");
+        $this->newOpportunity->setStatus(Entity::STATUS_DRAFT);
+        $app->em->persist($this->newOpportunity);
         $app->em->flush();
 
-        return $newOpportunity;
+        return $this->newOpportunity;
     }
 
-    private function duplicateEvaluationMethods(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicateEvaluationMethods() : void
     {
         $app = App::i();
 
         // duplica o método de avaliação para a oportunidade primária
         $evaluationMethodConfigurations = $app->repo('EvaluationMethodConfiguration')->findBy([
-            'opportunity' => $opportunity
+            'opportunity' => $this->opportunity
         ]);
         foreach ($evaluationMethodConfigurations as $evaluationMethodConfiguration) {
             $newMethodConfiguration = clone $evaluationMethodConfiguration;
-            $newMethodConfiguration->setOpportunity($newOpportunity);
+            $newMethodConfiguration->setOpportunity($this->newOpportunity);
             $newMethodConfiguration->save(true);
 
             // duplica os metadados das configurações do modelo de avaliação
@@ -67,17 +72,17 @@ trait EntityOpportunityDuplicator {
         }
     }
 
-    private function duplicatePhases(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicatePhases() : void
     {
         $app = App::i();
 
         $phases = $app->repo('Opportunity')->findBy([
-            'parent' => $opportunity
+            'parent' => $this->opportunity
         ]);
         foreach ($phases as $phase) {
             if (!$phase->getMetadata('isLastPhase')) {
                 $newPhase = clone $phase;
-                $newPhase->setParent($newOpportunity);
+                $newPhase->setParent($this->newOpportunity);
 
                 // duplica os metadados das fases
                 foreach ($phase->getMetadata() as $metadataKey => $metadataValue) {
@@ -108,59 +113,68 @@ trait EntityOpportunityDuplicator {
         }
     }
 
-    private function duplicateMetadata(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicateMetadata() : void
     {
-        foreach ($opportunity->getMetadata() as $metadataKey => $metadataValue) {
+        foreach ($this->opportunity->getMetadata() as $metadataKey => $metadataValue) {
             if (!is_null($metadataValue) && $metadataValue != '') {
-                $newOpportunity->setMetadata($metadataKey, $metadataValue);
+                $this->newOpportunity->setMetadata($metadataKey, $metadataValue);
             }
         }
 
-        $newOpportunity->setTerms(['area' => $opportunity->terms['area']]);
-        $newOpportunity->setTerms(['tag' => $opportunity->terms['tag']]);
-        $newOpportunity->saveTerms();
+        $this->newOpportunity->setTerms(['area' => $this->opportunity->terms['area']]);
+        $this->newOpportunity->setTerms(['tag' => $this->opportunity->terms['tag']]);
+        $this->newOpportunity->saveTerms();
     }
    
-    private function duplicateRegistrationFieldsAndFiles(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicateRegistrationFieldsAndFiles() : void
     {
-        foreach ($opportunity->getRegistrationFieldConfigurations() as $registrationFieldConfiguration) {
+        foreach ($this->opportunity->getRegistrationFieldConfigurations() as $registrationFieldConfiguration) {
             $fieldConfiguration = clone $registrationFieldConfiguration;
-            $fieldConfiguration->setOwnerId($newOpportunity->getId());
+            $fieldConfiguration->setOwnerId($this->newOpportunity->getId());
             $fieldConfiguration->save(true);
         }
 
-        foreach ($opportunity->getRegistrationFileConfigurations() as $registrationFileConfiguration) {
+        foreach ($this->opportunity->getRegistrationFileConfigurations() as $registrationFileConfiguration) {
             $fileConfiguration = clone $registrationFileConfiguration;
-            $fileConfiguration->setOwnerId($newOpportunity->getId());
+            $fileConfiguration->setOwnerId($this->newOpportunity->getId());
             $fileConfiguration->save(true);
         }
 
     }
 
-    private function duplicateMetalist(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicateMetalist() : void
     {
-        foreach ($opportunity->getMetaLists() as $metaList_) {
+        foreach ($this->opportunity->getMetaLists() as $metaList_) {
             foreach ($metaList_ as $metaList__) {
                 $metalist = clone $metaList__;
-                $metalist->setOwner($newOpportunity);
+                $metalist->setOwner($this->newOpportunity);
             
                 $metalist->save(true);
             }
         }
     }
 
-    private function duplicateFiles(ProjectOpportunity $opportunity, ProjectOpportunity $newOpportunity) : void
+    private function duplicateFiles() : void
     {
         $app = App::i();
 
         $opportunityFiles = $app->repo('OpportunityFile')->findBy([
-            'owner' => $opportunity
+            'owner' => $this->opportunity
         ]);
 
         foreach ($opportunityFiles as $opportunityFile) {
             $newMethodOpportunityFile = clone $opportunityFile;
-            $newMethodOpportunityFile->owner = $newOpportunity;
+            $newMethodOpportunityFile->owner = $this->newOpportunity;
             $newMethodOpportunityFile->save(true);
+        }
+    }
+
+    private function duplicateAgentRelations() : void
+    {
+        foreach ($this->opportunity->getAgentRelations() as $agentRelation_) {
+            $agentRelation = clone $agentRelation_;
+            $agentRelation->owner = $this->newOpportunity;
+            $agentRelation->save(true);
         }
     }
 }

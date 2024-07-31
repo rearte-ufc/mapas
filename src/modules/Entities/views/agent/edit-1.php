235f005853d2127178126c2aec99c7bd00b7b3d9
PaG0Dezaum2005 <heitor.hes@hotmail.com>
2024-07-31 20:09:19 -0300

    ajeites mais um accordion

--- src/modules/Entities/views/agent/edit-1.php
+++ src/modules/Entities/views/agent/edit-1.php
@@ -45,22 +45,84 @@ $this->breadcrumb = [
 
     <mc-tabs class="tabs" sync-hash>
         <?php $this->applyTemplateHook('tabs','begin') ?>
-        <mc-tab label="<?= i::_e('Informações') ?>" slug="info">
-            <mc-container>
-                <entity-status :entity="entity"></entity-status>
-                <label><?php i::_e("Campos marcados com <span style='color:red;'>*</span> são de preenchimento obrigatório.") ?></label>
-                <mc-card class="feature__full">
-                    <template #title>
-                        <h3><?php i::_e("Informações de Apresentação") ?></h3>
-                    </template>
-                    <template #content>
-                        <div class="grid-12">
-                            <p class="col-12 mc-card__card_info"><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários") ?></p>
-                            <entity-cover :entity="entity" classes="col-12"></entity-cover>
-                            <div class="col-12 grid-12">
-                                <div class="col-3 sm:col-12 card_profile">
-                                    <entity-profile :entity="entity" :label="false"></entity-profile>
-                                </div>
+        <mc-tab label="<?= i::_e('Meu perfil') ?>" slug="info"> <!-- mudar o slug -->
+            <mc-container class="container">
+                <label class="entity-cards__label"><?php i::_e("Campos marcados com <span style='color:red;'>*</span> são de preenchimento obrigatório.") ?></label>
+            </mc-container>
+            <mc-container class="edit--profile">
+                <mc-accordion class="accordion-edit--profile">
+                    <entity-status :entity="entity"></entity-status>
+                    <!-- <mc-card class="feature__full"> -->
+                        <template #title>
+                            <h3><?php i::_e("Informações de Apresentação") ?></h3>
+                        </template>
+                        <template #content>
+                            <div class="grid-12 black-content">
+                                <p class="col-12 mc-card__card_info"><?php i::_e("Os dados inseridos abaixo serão exibidos para todos os usuários") ?></p>
+                                <entity-cover :entity="entity" classes="col-12"></entity-cover>
+                                <!-- <div class="col-12 grid-12"> -->
+                                    <!-- <div class="entity-profile"> -->
+                                        <entity-profile :entity="entity" :label="false"></entity-profile>
+                                    <!-- </div> -->
+                                <!-- </div> -->
+                                <?php $this->applyTemplateHook('entity-info','begin') ?>
+                                <entity-field :entity="entity" classes="col-12" prop="name" label="<?php i::_e('Nome de perfil') ?>"></entity-field>
+                                <?php $this->applyTemplateHook('entity-info','end') ?>
+                                <?php $this->applyTemplateHook('edit1-entity-info-taxonomie-area','before') ?>
+                                <entity-terms :entity="entity" taxonomy="area" editable classes="col-12" title="<?php i::_e('Área(s) de atuação *'); ?>"></entity-terms>
+                                <?php $this->applyTemplateHook('edit1-entity-info-taxonomie-area','after') ?>
+                                <entity-terms :entity="entity" taxonomy="funcao" editable classes="col-12" title="<?php i::_e('Função(ões) na cultura'); ?>"></entity-terms>
+                                <entity-terms :entity="entity" taxonomy="tag" classes="col-12" title="Tags" editable></entity-terms>
+                
+                                <?php $this->applyTemplateHook('edit1-entity-info-shortDescription','before') ?>
+                                <entity-field :entity="entity" classes="col-12" prop="shortDescription" label="<?php i::_e('Descrição curta') ?>"></entity-field>
+                                <?php $this->applyTemplateHook('edit1-entity-info-shortDescription','after') ?>
+                                <entity-field :entity="entity" classes="col-12" prop="longDescription" editable></entity-field>
+                
+                                <div class="col-12 black-content">Site</div>
+                                <?php $this->applyTemplateHook('edit1-entity-info-site','before') ?>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="site"></entity-field>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="siteDescription"></entity-field>
+                                <?php $this->applyTemplateHook('edit1-entity-info-site','after') ?>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="emailPublico" label="<?= i::__('E-mail público') ?>"></entity-field>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefonePublico" label="<?= i::__('Telefone público (com DDD)') ?>"></entity-field>
+                            </div>
+                        </template>
+                    <!-- </mc-card> -->
+                </mc-accordion>
+            </mc-container>
+            <mc-container class="edit--profile">
+                <mc-accordion class="accordion-edit--profile">
+                    <!-- <mc-card class="feature__full"> -->
+                        <template #title>
+                            <h3 class="bold"><?php i::_e("Dados Pessoais"); ?></h3>
+                
+                        </template>
+                        <template #content>
+                            <div class="grid-12 black-content">
+                                <p class="col-12 mc-card__card_info"><?php i::_e("Os dados inseridos abaixo <strong>não</strong> serão exibidos publicamente, exceto os casos em que forem selecionadas as opções ”Mostrar no perfil”."); ?></p>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="nomeSocial" label="<?= i::__('Nome Social') ?>"></entity-field>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="nomeCompleto" label="<?= i::__('Nome Completo') ?>"></entity-field>
+                                <entity-field v-if="global.auth.is('admin')" :entity="entity" prop="type" @change="entity.save(true).then(() => global.reload())" classes="col-12 sm:col-12"></entity-field>
+                                <entity-field :entity="entity" classes="col-4 sm:col-12" prop="cpf"></entity-field>
+                                <!-- <entity-field :entity="entity" classes="col-12" prop="cnpj" label="<?= i::__('MEI (CNPJ do MEI)') ?>"></entity-field> -->
+                                <entity-field :entity="entity" classes="col-4 sm:col-12" prop="emailPrivado" label="<?= i::__('E-mail pessoal') ?>"></entity-field>
+                                <entity-field :entity="entity" classes="col-4 sm:col-12" prop="telefone1" label="<?= i::__('Telefone pessoal (com DDD)') ?>"></entity-field>
+                                <!-- <entity-field :entity="entity" classes="col-6 sm:col-12" prop="telefone2" label="<?= i::__('Telefone privado 2 com DDD') ?>"></entity-field> -->
+                                <div class="divider col-12"></div>
+                                <entity-location :entity="entity" classes="col-12 sm:col-12" editable></entity-location>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="agenteItinerante" label="<?= i::__('É agente itinerante?') ?>"></entity-field>
+                            </div>
+                            <div class="divider divider--data"></div>
+                
+                            <div class="grid-12 black-content">
+                                <h4 class="col-12"><?php i::_e("<strong>Dados bancários</strong>"); ?></h4>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_account_type"></entity-field>
+                                <entity-field :entity="entity" classes="col-6 sm:col-12" prop="payment_bank_number"></entity-field>
+                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_account_number"></entity-field>
+                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_account_number"></entity-field>
+                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_branch"></entity-field>
+                                <entity-field :entity="entity" classes="col-3 sm:col-12" prop="payment_bank_dv_branch"></entity-field>
                             </div>
 
                             <?php $this->applyTemplateHook('entity-info','begin') ?>

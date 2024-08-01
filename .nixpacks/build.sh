afd2737a1dd4374beff2327c139729432ea624c2
Lucas Pirola <lucaspirola@gmail.com>
2024-08-01 00:40:19 -0300

    Adiciona pipeline para checagem dos testes de integração

--- .nixpacks/build.sh
+++ .nixpacks/build.sh
@@ -1,1 +1,1 @@
-docker build . -f ./.nixpacks/Dockerfile -t 745e5307-a768-47a8-9ade-d17560f8ae49 --build-arg NIXPACKS_METADATA=php --build-arg NIXPACKS_PHP_FALLBACK_PATH=/app/public/index.php --build-arg NIXPACKS_PHP_ROOT_DIR=/app/public --build-arg PORT=80
+docker build . -f ./.nixpacks/Dockerfile -t f221462a-2651-4673-8b92-64aaa0da5e22 --build-arg NIXPACKS_METADATA=php --build-arg NIXPACKS_PHP_FALLBACK_PATH=/app/public/index.php --build-arg NIXPACKS_PHP_ROOT_DIR=/app/public --build-arg PORT=80

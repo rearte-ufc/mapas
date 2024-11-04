47e876502755eee53338673075a49209ed9e6447
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-11-04 19:06:03 -0300

    feat: limpa campos quando entregas vinculadas a meta são desativadas

--- src/modules/OpportunityWorkplan/components/opportunity-enable-workplan/script.js
+++ src/modules/OpportunityWorkplan/components/opportunity-enable-workplan/script.js
@@ -39,6 +39,11 @@ app.component('opportunity-enable-workplan', {
                 this.entity.workplan_deliveryMaximumNumberOfDeliveries = 0;
             }
         },
+        'entity.workplan_deliveryReportTheDeliveriesLinkedToTheGoals'(_new){
+            if (!_new) {
+                this.disabledDeliveries();
+            }
+        },
     },
     methods: {
         autoSave(){
@@ -47,26 +52,33 @@ app.component('opportunity-enable-workplan', {
         disabledWorkPlan(){
             this.entity.workplan_dataProjectlimitMaximumDurationOfProjects = false;
             this.entity.workplan_dataProjectmaximumDurationInMonths = 0;
+
             this.entity.workplan_metaInformTheStageOfCulturalMaking = false;
             this.entity.workplan_metaInformTheValueGoals = false;
             this.entity.workplan_metaLimitNumberOfGoals = false;
             this.entity.workplan_metaMaximumNumberOfGoals = 0;
+            this.entity.workplan_metaReportTheBudgetAction = false;
+
             this.entity.workplan_deliveryReportTheDeliveriesLinkedToTheGoals = false;
+            
+            this.disabledDeliveries();
+        },
+        disabledDeliveries() {
             this.entity.workplan_deliveryLimitNumberOfDeliveries = false;
             this.entity.workplan_deliveryMaximumNumberOfDeliveries = 0;
-            this.entity.workplan_monitoringInformDeliveryType = [];
             this.entity.workplan_registrationReportTheNumberOfParticipants = false;
             this.entity.workplan_registrationReportExpectedRenevue = false;
             this.entity.workplan_registrationInformActionPAAR = false;
+            this.entity.workplan_registrationInformCulturalArtisticSegment = false;
+
+            this.entity.workplan_monitoringInformTheFormOfAvailability = false;
+            this.entity.workplan_monitoringEnterDeliverySubtype = false;
+            this.entity.workplan_monitoringInformDeliveryType = [];
             this.entity.workplan_monitoringInformAccessibilityMeasures = false;
             this.entity.workplan_monitoringInformThePriorityTerritories = false;
             this.entity.workplan_monitoringProvideTheProfileOfParticipants = false;
             this.entity.workplan_monitoringInformThePriorityAudience = false;
-            this.entity.workplan_monitoringReportExecutedRevenue = false;   
-            this.entity.workplan_metaReportTheBudgetAction = false;
-            this.entity.workplan_registrationInformCulturalArtisticSegment = false;
-            this.entity.workplan_monitoringInformTheFormOfAvailability = false;
-            this.entity.workplan_monitoringEnterDeliverySubtype = false;
-        }
+            this.entity.workplan_monitoringReportExecutedRevenue = false; 
+        },
     },
 })

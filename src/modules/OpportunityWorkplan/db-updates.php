a4196b65990800b65dbb155545dcd2d40903f17b
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-11-07 18:53:33 -0300

    feat: cria db updates do plano de trabalho

--- src/modules/OpportunityWorkplan/db-updates.php
+++ src/modules/OpportunityWorkplan/db-updates.php
@@ -10,44 +10,65 @@ return [
     'create table workplan' => function () {
         __exec("CREATE TABLE registration_workplan (
           id SERIAL PRIMARY KEY,
+          agent_id INTEGER,          
           registration_id INTEGER,
-          project_duration INTEGER,
-          cultural_artistic_segment VARCHAR(50)
+          create_timestamp timestamp without time zone NOT NULL,
+          update_timestamp timestamp(0) without time zone
       )");
 
         __exec("ALTER TABLE registration_workplan ADD FOREIGN KEY (registration_id) REFERENCES registration(id)");
+        __exec("ALTER TABLE registration_workplan ADD FOREIGN KEY (agent_id) REFERENCES agent(id)");
     },
-    'create table workplan goals' => function () {
+    'create table workplan_meta' => function () {
+        __exec("CREATE TABLE public.registration_workplan_meta (
+            object_id integer NOT NULL,
+            key character varying(255) NOT NULL,
+            value text,
+            id SERIAL NOT NULL
+        );");
+        __exec("ALTER TABLE registration_workplan_meta ADD FOREIGN KEY (object_id) REFERENCES registration_workplan(id)");
+    },
+
+    'create table workplan goal' => function () {
         __exec("CREATE TABLE registration_workplan_goal (
-          id SERIAL PRIMARY KEY,
-          workplan_id INTEGER,
-          month_initial VARCHAR(50),
-          month_end VARCHAR(50),
-          title VARCHAR(255),
-          description TEXT,
-          cultural_making_stage VARCHAR(50),
-          amount numeric(15, 2)
-      )");
+            id SERIAL PRIMARY KEY,
+            agent_id INTEGER,          
+            workplan_id INTEGER,
+            create_timestamp timestamp without time zone NOT NULL,
+            update_timestamp timestamp(0) without time zone
+        )");
 
         __exec("ALTER TABLE registration_workplan_goal ADD FOREIGN KEY (workplan_id) REFERENCES registration_workplan(id)");
+        __exec("ALTER TABLE registration_workplan_goal ADD FOREIGN KEY (agent_id) REFERENCES agent(id)");
     },
+    'create table workplan_goal meta' => function () {
+        __exec("CREATE TABLE public.registration_workplan_goal_meta (
+              object_id integer NOT NULL,
+              key character varying(255) NOT NULL,
+              value text,
+              id SERIAL NOT NULL
+          );");
+        __exec("ALTER TABLE registration_workplan_goal_meta ADD FOREIGN KEY (object_id) REFERENCES registration_workplan_goal(id)");
+    },
+    'create table workplan goal delivery' => function () {
+        __exec("CREATE TABLE registration_workplan_goal_delivery (
+            id SERIAL PRIMARY KEY,
+            agent_id INTEGER,     
+            goal_id INTEGER,     
+            create_timestamp timestamp without time zone NOT NULL,
+            update_timestamp timestamp(0) without time zone
+        )");
 
-    'create table deliveries' => function () {
-      __exec("CREATE TABLE registration_workplan_goal_delivery (
-        id SERIAL PRIMARY KEY,
-        goal_id INTEGER,
-        name VARCHAR(255),
-        description VARCHAR(255),
-        type VARCHAR(50),
-        segment_delivery VARCHAR(50),
-        budget_action VARCHAR(50),
-        expected_number_people INTEGER,
-        generate_revenue BOOLEAN,
-        renevue_qtd INTEGER,
-        unit_value_forecast numeric(15, 2),
-        total_value_forecast numeric(15, 2)
-    )");
-
-      __exec("ALTER TABLE registration_workplan_goal_delivery ADD FOREIGN KEY (goal_id) REFERENCES registration_workplan_goal(id)");
-  },
+        __exec("ALTER TABLE registration_workplan_goal_delivery ADD FOREIGN KEY (goal_id) REFERENCES registration_workplan_goal(id)");
+        __exec("ALTER TABLE registration_workplan_goal_delivery ADD FOREIGN KEY (agent_id) REFERENCES agent(id)");
+    },
+    'create table workplan_goal delivery meta' => function () {
+        __exec("CREATE TABLE public.registration_workplan_goal_delivery_meta (
+              object_id integer NOT NULL,
+              key character varying(255) NOT NULL,
+              value text,
+              id SERIAL NOT NULL
+          );");
+        __exec("ALTER TABLE registration_workplan_goal_delivery_meta ADD FOREIGN KEY (object_id) REFERENCES registration_workplan_goal_delivery(id)");
+    },
 ];
